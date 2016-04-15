41d1299222dd5f9a9d8bb6b71b4b571e9a7b4489
olezhek85 <olezhek_85@list.ru>
2016-04-15 21:34:11 +0300

    Change fake data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
-1000.times do
+100.times do
   Invoice.create(
     amount: Faker::Number.between(50_000, 100_000),
     company: Faker::Company.name,
     contragent: Faker::Company.name,
     currency: 'USD',
-    date: Date.today,
+    date: Faker::Date.between(1.year.ago, Date.today),
     created_at: DateTime.now,
     updated_at: DateTime.now
   )
-end 
+end
